9d6488e7cf261a6efa2d4dd3550b2475b1a91347
Arseniy-Shablyko <241208arseniy@gmail.com>
2026-01-13 18:48:49 +0300

    Add files via upload

--- get-weather.js
+++ get-weather.js
@@ -1,7 +1,14 @@
-(async function getWeather(){
-    const link = await fetch('fb3c50c0fd9d5eadec97c52118809f7f')
-    const info = await response.json()
-    const Weather = info['Weather']
-    const WeatherNode = document.getElementById('WeatherInfo')
-    WeatherNode.textContent = {Weather};
-})
+const apiKey = 'e7a34f2f13c7a941877df88a4ddc7312';
+const city = 'Minsk';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+
+
+  (async function getWeather(){
+    const response = await fetch(url)
+    console.log(response);
+    const data = await response.json()
+    console.log(data);
+    const currencyNode  = document.getElementById('WeatherInfo')
+    currencyNode.textContent = `${data.name} : ${data.main.temp}°C`;
+})()
